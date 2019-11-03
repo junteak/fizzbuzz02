f206e317e8bf75a88aad2231448bc308648dafed
juntanaka <edamame.zombie@gmail.com>
2019-11-03 11:44:50 +0900

    全ての倍数を実装

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,24 @@
-number = 3
 
 
 def fizzbuzz_covert(number):
     if number % 3 == 0:
         return 'Fizz'
 
+    if number % 5 == 0:
+        return 'Buzz'
 
-print(fizzbuzz_covert(number))
+    if number % 15 == 0:
+        return 'FizzBuzz'
 
+    return str(number)
+
+
+for number in range(1, 101):
+    print(fizzbuzz_covert(number))
+
+# アラートを出すための。定義。
 assert fizzbuzz_covert(3) == 'Fizz'
+
+assert fizzbuzz_covert(5) == 'Buzz'
+
+assert fizzbuzz_covert(15) == 'FizzBuzz'
